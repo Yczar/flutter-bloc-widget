f839675a1236829439bc71feb9fba55594d65cfb
Yczar <babalolagbogo@gmail.com>
2023-04-09 21:20:04 +0100

    chore: add implementation for flutter vloc widget

--- lib/src/flutter_bloc_widget.dart
+++ lib/src/flutter_bloc_widget.dart
@@ -1,7 +1,54 @@
-/// {@template flutter_bloc_widget}
-/// My new Flutter package
-/// {@endtemplate}
-class FlutterBlocWidget {
-  /// {@macro flutter_bloc_widget}
-  const FlutterBlocWidget();
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// A widget that subscribes to a BLoC and rebuilds itself when the BLoC's
+/// state changes.
+abstract class FlutterBlocWidget<B extends BlocBase<S>, S>
+    extends StatefulWidget {
+  /// FlutterBlocWidget Constructor
+  const FlutterBlocWidget({super.key});
+
+  /// The BLoC to subscribe to.
+  B get bloc;
+
+  /// Builds the widget's UI based on the BLoC's state.
+  @protected
+  Widget build(BuildContext context, S state);
+
+  @override
+
+  /// This function returns an instance of the `_FlutterBlocWidgetState` class.
+
+  State<FlutterBlocWidget<B, S>> createState() =>
+      _FlutterBlocWidgetState<B, S>();
+}
+
+class _FlutterBlocWidgetState<B extends BlocBase<S>, S>
+    extends State<FlutterBlocWidget<B, S>> {
+  late final B _bloc = widget.bloc;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<S>(
+      stream: _bloc.stream,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return widget.build(context, snapshot.data as S);
+        } else {
+          return const SizedBox();
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _bloc.close();
+    super.dispose();
+  }
 }
